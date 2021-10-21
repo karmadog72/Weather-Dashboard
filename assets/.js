eb3ac8a3461f4274faecc5ab40d81760afb0b019
Danika Pearson <89046934+karmadog72@users.noreply.github.com>
2021-10-20 18:40:40 -0600

    test

---
+++ assets/.js
@@ -0,0 +1,16 @@
+var searchCity = document.querySelector("")
+var cityName = $('searchCity').val()
+var APIKey = "6d385c1b5e0b0ca70a1427d6269e84a1"
+var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + &gitappid=6d385c1b5e0b0ca70a1427d6269e84a1";
+ fetch(apiUrl)
+ .then(function(response) {
+
+ }
+
+{
+"lat": 40.66,
+"lon": -112.01,
+"timezone-offset": "America/Utah",
+"timezone_offset": -
+
+}
