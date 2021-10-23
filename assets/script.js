de8349dde84c947a542bf74084fe5e2933c89580
Danika Pearson <89046934+karmadog72@users.noreply.github.com>
2021-10-23 10:37:51 -0600

    fixed uvi

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,8 @@ var temp = document.getElementById("temp");
 var humidity = document.getElementById("humidity");
 var speed = document.getElementById("wind");
 var uvi = document.getElementById("uvi");
-// var lat = document.getElementById("lat");
-// var lon = document.getElementById("lon");
+var lat;
+var lon;
 var current = document.getElementById("currentConditions");
 var APIKey = "6d385c1b5e0b0ca70a1427d6269e84a1";
 
@@ -24,26 +24,29 @@ function getWeather(e) {
       console.log("Humidity is:", data.main.humidity);
       console.log("speed is:", data.wind.speed);
 
-      current.textContent = data.name;
-      temp.textContent = data.main.temp + ".℉";
-      humidity.textContent = data.main.humidity + "% -Humidity";
-      wind.textContent = data.wind.speed + "-MPH";
-
-      //   var uvi = document.getElementById("uvi").value;
-      //   var uvi = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
-      //   fetch(apiUrl)
-      // .then(function (response) {
-      //   return response.json();
-      // })
-      // .then(function (data) {
-      //   console.log("uvi is:", data.main.uvi);
-      //   uvi.textContent = data.current.uvi;
+      current.textContent = "CITY-" + data.name;
+      temp.textContent = "🌡️TEMP-" + data.main.temp + " ℉";
+      humidity.textContent = "💦HUMIDITY-" + data.main.humidity + " % ";
+      wind.textContent = "💨WIND-" + data.wind.speed + "/MPH";
+      lat = data.coord.lat;
+      lon = data.coord.lon;
+      getForecast();
     });
 
-  // "lat": 40.66,
-  // "lon": -112.01,
+  function getForecast(e) {
+    var forecastUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+    fetch(forecastUrl)
+      .then(function (response2) {
+        return response2.json();
+      })
+      .then(function (data) {
+        console.log("uvi is:", data);
+        console.log(uvi);
+        uvi.textContent = "🌞UVI-" + data.current.uvi;
+      });
 
+    document.getElementById("uvi").value = "";
+  }
   document.getElementById("cityname").value = "";
 }
-
 searchBtn.addEventListener("click", getWeather);
